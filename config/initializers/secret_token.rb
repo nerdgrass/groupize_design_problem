fed502f3d841246f12476c21ccebd94d6822ea51
Catherine Powell <catherine@abakas.com>
2013-07-18 18:56:02 -0400

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupizeDesignerApp::Application.config.secret_token = '9fab064516f9806c70888a5d57678ef8368a444dbd4d2f0cc4013a009c6e1a0434c7957d90e8b99bb44ebadaf5f2a1fe631c8628d5e8101a99b4fc6a0fa678e4'
